f181d663f8708abcc8af0069e620bd8538abb488
Mekuanint Legese <mekuanint1212@gmail.com>
2023-03-10 09:11:25 +0300

    search result

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 document.getElementById('search-input').addEventListener('keyup', async (e) => {
-    // Search comments
-    // Use this API: https://jsonplaceholder.typicode.com/comments?postId=3
-    // Display the results in the UI
+    // selecting elements from the DOM
+    const searchResults = document.getElementById('search-results');
+    const errorMessage = document.getElementById('error-message');
 
-    // Things to look out for
-    // ---
-    // Use es6
-    // Error handling
+    try {
+        errorMessage.textContent = '';
+        
+        const userInput = e.target.value;
 
-})
+        // Clear the searchResults element if userInput is empty
+        if (userInput.trim() === '') {
+            searchResults.innerHTML = '';
+            return;
+        }
+
+        const response = await fetch('https://jsonplaceholder.typicode.com/comments?postId=3');
+        const results = await response.json();
+
+        const filteredResults = results.filter((result) => {
+            return result.name.toLowerCase().includes(userInput.toLowerCase())
+        });
+
+        if (filteredResults.length === 0) {
+            searchResults.textContent = 'No results are found with this name';
+        } else {
+            searchResults.innerHTML = ''; 
+            filteredResults.forEach((result) => {
+                const resultElement = document.createElement('div');
+                resultElement.textContent = result.name;
+                searchResults.appendChild(resultElement);
+            });
+        }
+
+    } catch (error) {
+        errorMessage.textContent = error.message;
+    }
+});
